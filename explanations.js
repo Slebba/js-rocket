673d378a4161fba39086442d501df2df7b97cbd1
slebba <ebba.hedenblad@gmail.com>
2016-11-20 19:55:38 +0100

    Added style sheet

---
+++ explanations.js
@@ -0,0 +1,33 @@
+// Explanations
+var favColor = 'red';
+var myFavColors = ['blue', 'red', 'green'];
+var nymOfFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	firstName: 'Rich',
+	lastName: 'Object',
+	favColors: ['blue', 'red', 'green'],
+	yearsAlive: 134,
+	isMale: true
+};
+
+var whatIsMyFavColor = function  () {
+	return 'blue';
+
+}
+
+var doSomething = function  () {
+	console.log ('doSomething')
+
+
+}
+
+var number1 = 10;
+var numer2 = 100;
+
+if (number1 >= number1) {
+	// do something
+	alert ('male');
+} else {
+	alert ('false')
+}
